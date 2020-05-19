2a62db5d32edb946488896fefa996c1ace2bd470
audrenmp <audren.mp@gmail.com>
2020-05-19 15:07:23 +0200

    planes instance destroy on manager

--- src/game/classes/logic/BackgroundManager/index.ts
+++ src/game/classes/logic/BackgroundManager/index.ts
@@ -9,17 +9,70 @@ class BackgroundManager {
     private red: number = 0xff0b0b
     private white: number = 0xffffff;
     private speed: number = 60;
-
+    private currentPlanes: Array<Plane> = [];
+    private nextPlanes: Array<Plane> = [];
+    private scene: Phaser.Scene
+    private canvasWidth: number = 0;
+    private planesAssets: Array<Array<string>> = [
+        ["firstplane_nb"],
+        ["secondplane_nb"],
+        ["thirdplane_nb"],
+    ]
     constructor(
         scene: Phaser.Scene,
     ) {
+        this.scene = scene
+
+        this.canvasWidth = scene.sys.game.canvas.width;
+
         // @ts-ignore
         let mask = new Mask(scene, 600, 400, "mask", 0, this.pink)
         Align.left(mask)
 
-        let first_plane_nb = new Plane(scene, 600, 400, "firstplane_nb", 3, this.speed)
-        // let second_plane_nb = new Plane(scene, 600, 400, "secondplane_nb", 2)
-        // let third_plane_nb = new Plane(scene, 600, 400, "thirdplane_nb", 1,)
+        for (let planenb = 0; planenb < 3; planenb++) {
+            this.generatePlanes(planenb)
+        }
+    }
+
+    /**
+     * name
+     */
+    public generatePlanes(planenb: number) {
+        this.currentPlanes[planenb] = new Plane(
+            this.scene, 0, 0,
+            this.planesAssets[planenb][0],
+            planenb,
+            this.speed
+        )
+
+        this.initDestroy(this.currentPlanes[planenb])
+    }
+
+    public initDestroy(planeinstance: Plane) {
+        const timeToExitCanvas = this.calculateTimeToExit(planeinstance.width, planeinstance.scale, planeinstance.speed, this.canvasWidth)
+        console.log('timeToExitCanvas : ' + timeToExitCanvas)
+
+        setTimeout(() => {
+            console.log('destroy')
+            planeinstance.destroy(true)
+        }, timeToExitCanvas)
+    }
+
+    /**
+   * Helper fonction
+   *
+   * Permet de calculer en sec combien de temps met
+   * le plan à sortir du canvas
+   */
+    public calculateTimeToExit(
+        planeWidth: number,
+        planeScale: number,
+        planeSpeed: number,
+        canvasWidth: number
+    ): number {
+        const v = planeSpeed
+        const d = canvasWidth + (planeWidth * planeScale) - ((planeWidth * planeScale) / 2)
+        return (d / v) * 1000
     }
 }
 
--- src/game/classes/physic/Plane/index.ts
+++ src/game/classes/physic/Plane/index.ts
@@ -1,31 +1,33 @@
 import Align from '../../utils/align'
 
-export enum PlaneDepth { 'first' = 1, 'second' = 2, 'third' = 3 }
+// FIRST PLANE = 3
+// SECOND PLANE = 2
+// THIRD PLANE = 1
+const mappingPlanes = [
+    3, 2, 1
+]
 
 class Plane extends Phaser.GameObjects.Sprite {
-  private planeBody: Phaser.Physics.Arcade.Body;
-  private plane: PlaneDepth
-  private speed: number
+  public planeBody: Phaser.Physics.Arcade.Body;
+  public speed: number
+  public planeNb: number
 
-  // FIRST PLANE = 3
-  // SECOND PLANE = 2
-  // THIRD PLANE = 1
   constructor(
     scene: Phaser.Scene,
     x: number = 0,
     y: number = 0,
     texture: string = '',
-    plane: PlaneDepth,
-    speed: number = 1
+    plane: number,
+    globalspeed: number = 1
   ) {
     super(scene, x, y, texture);
 
-    let { width, height } = scene.sys.game.canvas;
+    this.planeNb = mappingPlanes[plane]
+    console.log(this.planeNb)
 
-    this.plane = plane
-    this.speed = speed
+    this.speed = globalspeed * this.planeNb
 
-    this.setDepth(plane)
+    this.setDepth(this.planeNb)
 
     scene.add.existing(this)
     scene.physics.add.existing(this)
@@ -36,35 +38,11 @@ class Plane extends Phaser.GameObjects.Sprite {
 
     this.planeBody = this.body as Phaser.Physics.Arcade.Body;
 
-    const timeToExitCanvas = this.calculateTimeToExit(this.width, this.scale, 3 * this.speed, width)
-    // console.log((timeToExitCanvas))
-    setTimeout(() => {
-      console.log('destroy')
-      this.destroy(true)
-    }, timeToExitCanvas)
-
     this.movePlane()
   }
 
   public movePlane() {
-    this.planeBody.setVelocityX(this.plane * (- this.speed));
-  }
-
-  /**
-   * Helper fonction
-   *
-   * Permet de calculer en sec combien de temps met
-   * le plan à sortir du canvas
-   */
-  public calculateTimeToExit(
-    planeWidth: number,
-    planeScale: number,
-    planeSpeed: number,
-    canvasWidth: number
-  ): number {
-    const v = planeSpeed
-    const d = canvasWidth + (planeWidth * planeScale) - ((planeWidth * planeScale) / 2)
-    return (d / v) * 1000
+    this.planeBody.setVelocityX(- this.speed);
   }
 }
 
