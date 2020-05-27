9310dff856861636e260cc24a013f621a5fc252a
audrenmp <audren.mp@gmail.com>
2020-05-27 15:57:39 +0200

    change Helpers to helpers

--- src/game/components/CharactersComponent/CharacterBis/index.ts
+++ src/game/components/CharactersComponent/CharacterBis/index.ts
@@ -1,4 +1,4 @@
-import SpineContainer from "../../../Helpers/SpineContainer/SpineContainer";
+import SpineContainer from "../../../helpers/SpineContainer/SpineContainer";
 
 class PhysicCharacter extends SpineContainer {
   public SpineContainer: ISpineContainer;
