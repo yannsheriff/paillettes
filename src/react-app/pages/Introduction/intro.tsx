5d9739ec5611a55403a84c98f361ae4d6da8c652
audrenmp <audren.mp@gmail.com>
2020-06-11 22:14:23 +0200

    css

--- src/react-app/pages/Introduction/intro.tsx
+++ src/react-app/pages/Introduction/intro.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import { gameConfig } from "../../../game/config";
 import stepEventEmitter from "../../../game/helpers/StepEventEmitter";
 import { StepEventType } from "../../../game/helpers/StepEventEmitter/gamepadListener";
+import { arrowL, arrowR } from "../../../game/assets/";
 
 export default class Game extends React.Component<any> {
   componentDidMount() {
     stepEventEmitter.on(
       StepEventType.stepdown,
       (...directions: Array<string>) => {
-        // console.log("Pressed : ", ...directions);
+        console.log("Pressed : ", ...directions);
       }
     );
     stepEventEmitter.on(
       StepEventType.stepup,
       (...directions: Array<string>) => {
-        // console.log("realased : ", ...directions);
+        console.log("realased : ", ...directions);
       }
     );
   }
@@ -33,15 +34,13 @@ export default class Game extends React.Component<any> {
         <div id="brille-cherie-footer">
           <p>
             longpress
-            <span className="arrow-left">
-              <img src="../../" alt="arrow-left"/>
-            </span>
+              <img src={arrowL} alt="arrow-left" />
             and
-            <span className="arrow-right"/>
+              <img src={arrowR} alt="arrow-right" />
             to start
           </p>
         </div>
       </div>
-    )
+    );
   }
 }
