212c42a7e0668ff06714a6a6b22d9a98031c5c6c
audrenmp <audren.mp@gmail.com>
2020-06-14 17:08:14 +0200

    clean assets

--- src/game/assets/assets/index.ts
+++ src/game/assets/assets/index.ts
@@ -72,7 +72,7 @@ export const mainSpritesheets: SpritesheetData[] = [
 
 export const mainSpines: SpineData[] = [
   { key: "dragqueen", path: "assets/spine/dragqueen/" },
-  { key: "godmother", path: "assets/spine/godmother/" },
+  // { key: "godmother", path: "assets/spine/godmother/" },
 ];
 
 export const mainMusic: MusicData[] = [
