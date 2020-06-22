0baec2dd8e299552a8948b936e4dd5874934ac92
audrenmp <audren.mp@gmail.com>
2020-06-22 20:02:08 +0200

    add difficulty

--- src/game/components/SheetMusicComponent/Score/index.ts
+++ src/game/components/SheetMusicComponent/Score/index.ts
@@ -4,27 +4,27 @@ import MainStateManager, { MainState } from "../../../states/main";
 class Score {
   private score: Phaser.GameObjects.Text;
   private subtitle: Phaser.GameObjects.Text;
-  private difficulty: Phaser.GameObjects.Text;
+  // private difficulty: Phaser.GameObjects.Text;
 
   constructor(scene: Phaser.Scene, x: number, y: number, scale: number) {
     const font = 20 * scale;
     const fontScore = 55 * scale;
     ScoreStateManager.getInstance().subscribe(this.updateScore);
-    MainStateManager.getInstance().subscribe((state: MainState) => {
-      this.difficulty.setText(state.difficulty.toString());
-    });
+    // MainStateManager.getInstance().subscribe((state: MainState) => {
+    //   this.difficulty.setText(state.difficulty.toString());
+    // });
 
-    this.difficulty = scene.add
-      .text(x - 100, y - 50, "0", {
-        fontFamily: "LondrinaSolid",
-        fontSize: "20px",
-        fontStyle: "",
-        color: "#fff",
-        fixedWidth: 300,
-        align: "center",
-      })
-      .setDepth(11)
-      .setText("0");
+    // this.difficulty = scene.add
+    //   .text(x - 100, y - 50, "0", {
+    //     fontFamily: "LondrinaSolid",
+    //     fontSize: "20px",
+    //     fontStyle: "",
+    //     color: "#fff",
+    //     fixedWidth: 300,
+    //     align: "center",
+    //   })
+    //   .setDepth(11)
+    //   .setText("0");
 
     this.subtitle = scene.add.text(x - 100, y, "", {
       fontFamily: "LondrinaSolid",
